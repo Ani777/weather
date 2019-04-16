035a15df50a9dbd9ce79e05469180bbf00a149b7
ani <ani.grigorian.85@gmail.com>
2019-04-16 23:41:37 +0400

    Updates

---
+++ precache-manifest.3a40e39aff1634d4bade109489022be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "adb0509fc79bfc41122c",
+    "url": "/weather/static/js/main.408b9eb6.chunk.js"
+  },
+  {
+    "revision": "4e05a107cf836d7cf8f7",
+    "url": "/weather/static/js/2.4d26d502.chunk.js"
+  },
+  {
+    "revision": "adb0509fc79bfc41122c",
+    "url": "/weather/static/css/main.e4768b7e.chunk.css"
+  },
+  {
+    "revision": "0ec8960f6e7c3a2ef02bf3d7d0df35d9",
+    "url": "/weather/index.html"
+  }
+];
